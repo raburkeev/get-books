6a13f7dd9e89cca5036d49abe9c036ea6f0e20dd
Roman Burkeev <raburkeev@gmail.com>
2022-08-21 18:54:55 +0300

    add books

--- src/App/layouts/books.jsx
+++ src/App/layouts/books.jsx
@@ -11,10 +11,23 @@ const Books = () => {
     }, [])
 
     return JSON.stringify(books) === '[]'
-        ? 'loading'
-        : books.map(book => (
-            <div key={book.id}>{ book.name }</div>
-        ))
+        ? <h1>Loading...</h1>
+        : <div className="book_cards_container py-3">
+            {books.map(book => (
+                <div className="card" key={book.id}>
+                    <img src={book.imgUrl} className="card-img-top" alt="pic"/>
+                    <div className="card-body d-flex m-0 p-0">
+                        <div className="d-flex flex-column">
+                            <span className="fs-6 m-0">{book.rate}</span>
+                            <span className="fs-6">{book.numberOfRatings}</span>
+                        </div>
+                        <span className="btn btn-primary btn-sm">
+                            {book.genre.name}
+                        </span>
+                    </div>
+                </div>
+            ))}
+        </div>
 }
 
 export default Books
